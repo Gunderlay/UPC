6cdce107830a2d5b5c54f5da0e05c0db0fe43081
Andy <44203905+Gunderlay@users.noreply.github.com>
2018-10-17 02:19:27 -0500

    prueba2

--- prueba.cpp
+++ prueba.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <conio.h>
+#include <string.h>
 using namespace std;
 
+class CAlumno{
+	string name;
+public:
+	CAlumno(string name = "Andy") : name(name){}
+	string getNombre(){
+		return this->name;
+	}
+};
 int main(){
 	cout<<"Hola Mundo";
-		return 0;
+	cout<<"Prueba"<<endl;
+	CAlumno Andy ;
+	cout<<Andy.getNombre();
+	return 0;
 }
